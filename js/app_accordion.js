ab30281414ba554ddee79d86c46cb2e27bb20904
Omar <omardanielesquivel@gmail.com>
2022-08-14 20:42:59 -0500

    First Commit

---
+++ js/app_accordion.js
@@ -0,0 +1,25 @@
+(() => {
+
+
+    'use strict';
+
+
+    const titles = document.querySelectorAll('.all_services__row');
+    const contents = document.querySelectorAll('.all_services__content');
+    
+    titles.forEach(function(element, index) {
+        element.addEventListener('click', () => {
+            if (contents[index].classList.contains('all_services__content-active')) {
+                contents[index].classList.remove('all_services__content-active');
+                titles[index].classList.remove('all_services__content-active');
+                console.log('crotolamo');
+            } else {
+                contents[index].classList.add('all_services__content-active');
+                titles[index].classList.add('all_services__content-active');
+                console.log('crotolamo');
+            }
+        });
+    });
+
+
+})();
